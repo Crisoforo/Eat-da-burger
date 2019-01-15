fc36376929c2312223234fe148306865a73d2818
Alejandro <alejandro.ortiz.pichardo@gmail.com>
2019-01-14 18:19:06 -0800

    modified burgers

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,8 @@
-INSERT INTO cats (name) VALUES ('Meeses');
-INSERT INTO cats (name) VALUES ('Bobbi');
-INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
-INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
-INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
-INSERT INTO cats (name) VALUES ('Owens');
+
+
+INSERT INTO burger (burger_name) VALUES ('BigMac');
+INSERT INTO burger (burger_name) VALUES ('Whopper Jr');
+INSERT INTO burger (burger_name, devoured) VALUES ('Royale with cheese', true);
+INSERT INTO burger (burger_name, devoured) VALUES ('Tacoburger', true);
+INSERT INTO burger (burger_name, devoured) VALUES ('Plain', true);
+INSERT INTO burger (burger_name) VALUES ('Double Bacon Smokehouse Quarter');
