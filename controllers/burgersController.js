7bb8dde00967f00c35e2a66ad8192f489cc15223
Alejandro <alejandro.ortiz.pichardo@gmail.com>
2019-01-14 23:04:04 -0800

    almost done

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -20,7 +20,7 @@ router.post("/api/burgers", function(req, res) {
   burgers.create([
     "burger_name", "devoured"
   ], [
-    req.body.name, req.body.devoured
+    req.body.burger_name, req.body.devoured
   ], function(result) {
     // Send back the ID of the new quote
     res.json({ id: result.insertId });
@@ -47,7 +47,7 @@ router.put("/api/burgers/:id", function(req, res) {
 router.delete("/api/burgers/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
-  cat.delete(condition, function(result) {
+  burgers.delete(condition, function(result) {
     if (result.affectedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
